d739cfcee3f702bf779fd6a3dcc69837845521c0
manis <manis@LAPTOP-3U4FJCRH>
2023-04-04 16:09:40 +0530

    second commit

---
+++ day/src/day/chardemo.java
@@ -0,0 +1,14 @@
+package day;
+
+public class chardemo {
+	public static void main(String[] args) {
+		char ch = 'a';
+		char ch1 = 70;
+		char ch2= '\u00A3';
+		System.out.println(ch);
+		System.out.println(ch1);
+		System.out.println(ch1);
+
+	}
+
+}
---
+++ day/src/day/datatype.java
@@ -0,0 +1,15 @@
+package day;
+
+public class datatype {
+
+	public static void main(String args[]) {
+		int num1=5/2;
+		float num2=13/45;
+		double num3=10d/3d;
+		float num4=14f/3f;
+		System.out.println("The value is: "+num1);
+		System.out.println("The value is: "+num2);
+		System.out.println("The value is: "+num3);
+		System.out.println("The value is: "+num4);
+	}
+}
