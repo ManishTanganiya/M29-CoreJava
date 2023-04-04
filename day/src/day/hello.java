88112dc8d232f65e31a0ea478a4c647f09ed869f
manis <manis@LAPTOP-3U4FJCRH>
2023-04-04 14:52:54 +0530

    first commit

---
+++ day/src/day/hello.java
@@ -0,0 +1,8 @@
+package day;
+
+public class hello {
+	public static void main(String args[]) {
+	System.out.print("hello");
+
+}
+}
---
+++ day/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manis
+ *
+ */
+module day {
+}
