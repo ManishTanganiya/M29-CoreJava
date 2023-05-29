a551eee64ae99859eea2a9a8032a09dbd8f5e521
manis <manis@LAPTOP-3U4FJCRH>
2023-05-29 22:24:50 +0530

    Twelve Commit

---
+++ daytwelve/src/org/tnsif/multithreading/ThreadExample.java
@@ -0,0 +1,15 @@
+//Program to demonstrate on Multithreading
+package org.tnsif.multithreading;
+
+public class ThreadExample {
+
+	public static void main(String[] args) {
+		// It return reference to currently executing thread object
+		Thread t=Thread.currentThread();
+		System.out.println("Current Thread: "+t);
+		
+		
+
+	}
+
+}
