37218a1568a721cd7afeee1d8f9794feaf2de623
manis <manis@LAPTOP-3U4FJCRH>
2023-06-06 15:15:58 +0530

    First commit

---
+++ daythirteen/src/org/tnsif/streamapi/MappingExample.java
@@ -0,0 +1,13 @@
+package org.tnsif.streamapi;
+import java.util.List;
+import java.util.Arrays;
+public class MappingExample {
+
+	public static void main(String[] args) {
+		// Using Collections
+		List<String>obj=Arrays.asList("Manish","Abhinav","Amit");
+		obj.stream().map(i->i.length()).forEach((i)-> System.out.println(i+" "));
+
+	}
+
+}
