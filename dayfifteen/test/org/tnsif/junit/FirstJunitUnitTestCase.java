5b58c360c3e4ea524d2b54cc583e13946dbc3665
manis <manis@LAPTOP-3U4FJCRH>
2023-06-08 16:11:58 +0530

    Fifteenth commit

---
+++ dayfifteen/test/org/tnsif/junit/FirstJunitUnitTestCase.java
@@ -0,0 +1,20 @@
+package org.tnsif.junit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+class FirstJunitUnitTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	@DisplayName("Simple Testing")
+	void display() {
+		System.out.println("First Junit Test case");
+	}
+
+	
+	
+
+}
